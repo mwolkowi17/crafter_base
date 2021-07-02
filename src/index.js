389b9a04c670b826f29b52df85055509fb529e03
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-07-02 23:27:28 +0200

    new file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-const a = 1;
+import * as THREE from 'three';
+import {cube} from './cube.js';
+import { light } from './hemi_light.js';
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+document.body.appendChild(renderer.domElement);
+
+scene.add(cube);
+scene.add(light);
+
+camera.position.z = 8;
+camera.position.y = 1.5;
+
+const animate = function () {
+    requestAnimationFrame(animate);
+
+    //cube.rotation.x += 0.02;
+    //cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+    
+    controls.update();
+
+};
+
+animate();
