48a3fc8c16e14551bb98270b742bb044f42cfdf0
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-07-04 10:16:48 +0200

    both siedes ready

--- src/cube_side.js
+++ src/cube_side.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 
 const width = 0.2;
-const height = 1;
+const height = 1.2;
 const depth = 1;
 const geometry = new THREE.BoxGeometry(width, height, depth);
 const material = new THREE.MeshPhongMaterial({ color: 0xb5651d });
