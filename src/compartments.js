31370957c776004f35a53c3e7480c4a8f05f8666
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-07-14 21:19:17 +0200

    compartment top added

--- src/compartments.js
+++ src/compartments.js
@@ -9,3 +9,7 @@ const material = new THREE.MeshPhongMaterial({ color: 0x787878 });
 export const compartment1 = new THREE.Mesh(geometry, material);
 compartment1.position.x=0;
 compartment1.position.y=0.5;
+
+export const compartment3 = new THREE.Mesh(geometry, material);
+compartment3.position.x=0;
+compartment3.position.y=2.5;
